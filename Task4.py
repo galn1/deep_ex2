ad98a54b252c10d8b4545ec61e761a59143d022c
galn1 <galnezer@gmail.com>
2018-06-18 20:27:46 +0300

    task 4 cerrected. files for 5a and 5b added

--- Task4.py
+++ Task4.py
@@ -1,10 +1,6 @@
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
-from tensorflow.contrib.learn.python.learn.datasets.mnist import DataSet
-import collections
-import numpy as np
 
-Datasets = collections.namedtuple('Datasets', ['train', 'validation', 'test'])
 tf.logging.set_verbosity(tf.logging.INFO)
 mode = "train"
 
@@ -14,7 +10,6 @@ def network_model(data):
     Runs the network
     Parameters:
         data (tf.placeholder): the input data
-        perform_dropout (boolean): when True the dropout is performed.
     Output:
         the result of the network
     """
@@ -76,9 +71,6 @@ def train_network(dataset):
     prediction = network_model(x)
     cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=prediction, labels=y))
 
-    #add this for task 5a
-    #learning_rate = tf.placeholder(tf.float32, shape=[])
-
     optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(cost)
     correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))
     accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
@@ -87,17 +79,12 @@ def train_network(dataset):
     last_validation_acc = 0
     n_bad_batches = 0
 
-    #add this for task 5b
-    #optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
         for epoch in range(n_epochs):
             mode = "train"
             epoch_loss = 0
             epoch_x, epoch_y = dataset.train.next_batch(batch_size)
-            #also this for 5a
-            # learning_rate_cut = 2 ** int((epoch+1)/400)
-            #_, c = sess.run([optimizer, cost], feed_dict={x: epoch_x, y: epoch_y, learning_rate: 0.001/learning_rate_cut})
             _, c = sess.run([optimizer, cost], feed_dict={x: epoch_x, y: epoch_y})
             epoch_loss += c
             tf.logging.info(epoch)
@@ -129,28 +116,7 @@ def train_network(dataset):
 
 if __name__ == '__main__':
     tf.logging.info("Loading MNIST dataset")
-    mnist = input_data.read_data_sets("MNIST_data/", one_hot=True, validation_size=0)
-
-    train_size = len(mnist.train.images)
-    test_size = len(mnist.test.images)
-    dataset_size = train_size + test_size
-    validation_size = int(dataset_size / 5)
-
-    after_permutation_indexes = np.random.permutation(train_size)
-
-    # validation set
-    validation_images = mnist.train.images[after_permutation_indexes[: validation_size]]
-    validation_labels = mnist.train.labels[after_permutation_indexes[: validation_size]]
-
-    # train set
-    train_images = mnist.train.images[after_permutation_indexes[validation_size:]]
-    train_labels = mnist.train.labels[after_permutation_indexes[validation_size:]]
-
-    train = DataSet(train_images, train_labels, reshape=False)
-    validation = DataSet(validation_images, validation_labels, reshape=False)
-
-    mnist_with_validation = Datasets(train=train, validation=validation, test=mnist.test)
-
+    mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
     tf.logging.info("Initiating Training")
-    train_network(mnist_with_validation)
+    train_network(mnist)
     tf.logging.info("Finished")
---
+++ Task5b.py
@@ -0,0 +1,122 @@
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+
+tf.logging.set_verbosity(tf.logging.INFO)
+mode = "train"
+
+
+def network_model(data):
+    """
+    Runs the network
+    Parameters:
+        data (tf.placeholder): the input data
+    Output:
+        the result of the network
+    """
+
+    # Input Layer.
+    input_layer = tf.reshape(data, [-1, 28, 28, 1])
+
+    # Convolutional Layer #1
+    conv1 = tf.layers.conv2d(inputs=input_layer, filters=32, kernel_size=[5, 5], padding="same")
+
+    # norm1
+    batch_norm1 = tf.layers.batch_normalization(inputs=conv1)
+
+    # Pooling Layer #1
+    pool1 = tf.layers.max_pooling2d(inputs=batch_norm1, pool_size=[2, 2], strides=2)
+
+    # Convolutional Layer #2
+    conv2 = tf.layers.conv2d(inputs=pool1, filters=64, kernel_size=[5, 5], padding="same")
+
+    # norm2
+    batch_norm2 = tf.layers.batch_normalization(inputs=conv2)
+
+    # Pooling Layer #1
+    pool2 = tf.layers.max_pooling2d(inputs=batch_norm2, pool_size=[2, 2], strides=2)
+
+    # Dense Layer #1
+    pool2_flat = tf.reshape(pool2, [-1, 7 * 7 * 64])
+    dense1 = tf.layers.dense(inputs=pool2_flat, units=1024, activation=tf.nn.relu)
+
+    dropout = tf.layers.dropout(inputs=dense1, rate=0.4, training=mode=="train")
+
+    # Dense Layer #2
+    dense2 = tf.layers.dense(inputs=dropout, units=1024, activation=tf.nn.relu)
+
+    # the dense for the softmax
+    predictions = tf.layers.dense(inputs=dense2, units=10)
+
+    return predictions
+
+
+def train_network(dataset):
+    """
+    trains the network
+    """
+    global mode
+    x = tf.placeholder('float', [None, 784])
+    y = tf.placeholder('float')
+
+    # batch size
+    batch_size = 100
+
+    # number of iterations
+    n_epochs = 5000
+
+    # the learning rate
+    learning_rate = 0.001
+
+    # runs the network
+    prediction = network_model(x)
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=prediction, labels=y))
+
+    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+    correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
+
+    # for the validation improvement checking
+    last_validation_acc = 0
+    n_bad_batches = 0
+
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        for epoch in range(n_epochs):
+            mode = "train"
+            epoch_loss = 0
+            epoch_x, epoch_y = dataset.train.next_batch(batch_size)
+            _, c = sess.run([optimizer, cost], feed_dict={x: epoch_x, y: epoch_y})
+            epoch_loss += c
+            tf.logging.info(epoch)
+            if (epoch + 1) % 250 == 0:
+                msg = 'Epoch ' + str(epoch+1) + ' completed out of ' + str(n_epochs) + ' loss: ' + str(epoch_loss)
+                tf.logging.info(msg)
+
+            # the validation accuracy
+            mode = "validation"
+            validation_acc = accuracy.eval({x: dataset.validation.images, y: dataset.validation.labels})
+            # checks for improvement
+            if validation_acc <= last_validation_acc:
+                n_bad_batches += 1
+            else:
+                # needs 3 consecutive batches. restarts the count
+                n_bad_batches = 0
+            # saves the validation accuracy
+            last_validation_acc = validation_acc
+            # when there was 3 consecutive bad batches
+            if n_bad_batches == 3:
+                tf.logging.info('Early stopping')
+                break
+
+        msg = 'Validation accuracy: ' + str(last_validation_acc)
+        tf.logging.info(msg)
+        mode = "test"
+        msg = 'Accuracy: ' + str(accuracy.eval({x: dataset.test.images, y: dataset.test.labels}))
+        tf.logging.info(msg)
+
+if __name__ == '__main__':
+    tf.logging.info("Loading MNIST dataset")
+    mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+    tf.logging.info("Initiating Training")
+    train_network(mnist)
+    tf.logging.info("Finished")
